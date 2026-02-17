78ed76f2997312a446efc8c34bfa300788d9b9bb
Thomas Isebeck <tisebeck@proton.me>
2026-02-17 15:43:05 +0200

    coded the node, still need to test

---
+++ GP/Node.cpp
@@ -0,0 +1,161 @@
+#include "Node.h"
+
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <memory>
+#include <string>
+using namespace std;
+
+// ------------------------ NODE ----------------------- //
+
+Node::Node(NodeType type) : type(type) {}
+
+NodeType Node::getType() const { return this->type; }
+
+// -------------------- CONSTANT NODE ------------------ //
+
+ConstantNode::ConstantNode(double value) : Node(CONSTANT), value(value) {}
+
+double ConstantNode::evaluate(const vector<double>& vars) {
+  // constant node does not care about variables
+  return this->value;
+}
+
+string ConstantNode::print(const vector<double>& vars) {
+  return std::to_string(this->value);
+}
+
+// -------------------- VARIABLE NODE ------------------ //
+
+VariableNode::VariableNode(int index) : index(index), Node(VARIABLE) {}
+
+double VariableNode::evaluate(const vector<double>& vars) {
+  // look up my value in the hashmap
+  return vars[this->index];
+}
+
+string VariableNode::print(const vector<double>& vars) {
+  return "x(" + std::to_string(vars[this->index]) + ")";
+}
+
+// -------------------- OPERATOR NODE ------------------ //
+
+OperatorNode::OperatorNode(OpType type) : Node(OPERATOR), type(type) {
+  // to check the assert
+  this->isUnary = this->type == OpType::SIN || this->type == OpType::SQRT ||
+		  this->type == OpType::SQUARE;
+}
+
+string convOpToString(OpType op) {
+  switch (op) {
+    case OpType::ADD:
+      return "ADD";
+    case OpType::SUB:
+      return "SUB";
+    case OpType::MUL:
+      return "MUL";
+    case OpType::DIV:
+      return "DIV";
+    case OpType::SQRT:
+      return "SQRT";
+    case OpType::SIN:
+      return "SIN";
+    case OpType::POW:
+      return "POW";
+    case OpType::SQUARE:
+      return "SQUARE";
+    default:
+      assert(false && "convOpToString: Unknown OpType");
+      return "UNKNOWN";
+  }
+}
+
+string OperatorNode::print(const vector<double>& vars) {
+  string childrenOut;
+
+  for (const auto& child : this->children) {
+    childrenOut += child->print(vars) + ",";
+  }
+
+  return convOpToString(this->type) + "( " +
+	 childrenOut.substr(0, childrenOut.length() - 1) + " )";
+}
+
+void OperatorNode::addChild(unique_ptr<Node> newChild) {
+  // adding the first child for unary, and first or second childe for binary
+  assert((this->isUnary && children.empty()) ||
+	 (!this->isUnary && children.size() < 2));
+
+  this->children.push_back(move(newChild));
+}
+
+double protectedDivide(double numerator, double denominator) {
+  if (denominator == 0) {
+    return 0;
+  }
+
+  return numerator / denominator;
+}
+
+double protectedSqrt(double num) {
+  if (num < 0) {
+    return 0;
+  }
+
+  return sqrt(num);
+}
+
+double protectedPow(double base, double exp) {
+  if (base < 0) {
+    return 0;
+  }
+  return pow(base, exp);
+}
+
+double OperatorNode::evaluate(const vector<double>& vars) {
+  // INFO: unary must have 1 child
+  // binay must have 2 children
+  assert((this->isUnary && children.size() == 1) ||
+	 (!this->isUnary && children.size() == 2));
+
+  // loop over the children and call evalute based on the operater
+  switch (this->type) {
+    case OpType::DIV: {
+      // protected divide, 2 children
+      return protectedDivide(this->children[0]->evaluate(vars),
+			     this->children[1]->evaluate(vars));
+    }
+
+    case OpType::ADD:
+      return this->children[0]->evaluate(vars) +
+	     this->children[1]->evaluate(vars);
+
+    case OpType::SQUARE: {
+      double val = this->children[0]->evaluate(vars);
+      return val * val;
+    }
+
+    case OpType::MUL:
+      return this->children[0]->evaluate(vars) *
+	     this->children[1]->evaluate(vars);
+
+    case OpType::POW:
+      // INFO: x to the power of n
+      return protectedPow(this->children[0]->evaluate(vars),
+			  this->children[1]->evaluate(vars));
+
+    case OpType::SIN:
+      return sin(this->children[0]->evaluate(vars));
+
+    case OpType::SQRT:
+      return protectedSqrt(this->children[0]->evaluate(vars));
+
+    case OpType::SUB:
+      return this->children[0]->evaluate(vars) -
+	     this->children[1]->evaluate(vars);
+
+    default:
+      assert(false && "OperatorNode::evaluate - Unhandled node type");
+  }
+}
---
+++ GP/Node.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+enum NodeType { OPERATOR, CONSTANT, VARIABLE };
+
+enum class OpType { ADD, SUB, MUL, DIV, SQRT, SIN, POW, SQUARE };
+
+class Node {
+ private:
+  NodeType type;
+
+ public:
+  Node(NodeType type);
+  [[nodiscard]] NodeType getType() const;
+
+  // variable names mapped to values ("a" -> 4.1)
+  virtual double evaluate(const std::vector<double>& vars) = 0;
+  virtual std::string print(const std::vector<double>& vars) = 0;
+  virtual ~Node() = default;
+};
+
+class VariableNode : public Node {
+ private:
+  int index;
+
+ public:
+  VariableNode(int index);
+  std::string print(const std::vector<double>& vars) override;
+  double evaluate(const std::vector<double>& vars) override;
+};
+
+// operator nodes have a type (oporand)
+// and children (who to apply this operand to)
+// when generating the tree
+class OperatorNode : public Node {
+ private:
+  std::vector<std::unique_ptr<Node>> children;
+  bool isUnary;
+  OpType type;
+
+ public:
+  OperatorNode(OpType type);
+  void addChild(std::unique_ptr<Node> newChild);
+  std::string print(const std::vector<double>& vars) override;
+  double evaluate(const std::vector<double>& vars) override;
+};
+
+// always a leaf node, no children
+// constant value
+class ConstantNode : public Node {
+ private:
+  double value;
+
+ public:
+  ConstantNode(double value);
+  std::string print(const std::vector<double>& vars) override;
+  double evaluate(const std::vector<double>& vars) override;
+};
